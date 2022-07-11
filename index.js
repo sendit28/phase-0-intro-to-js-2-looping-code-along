60ebf455f61c195a290cc27cd4b0bfc7009afd1f
+ <+>
2022-07-10 21:43:45 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, giftType) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) 
+    {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`)  
+    }
+    return cards
+}
+
+function countDown(number) {
+    let countDown = number
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+}
+   
+
+
+
+
+
